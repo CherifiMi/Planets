97aaa0d65392babd08789229fb3ff1c4f516f97f
CherifiMi <cherifi154949@gmail.com>
2022-07-02 21:00:52 +0100

    animation done need fixes

--- app/src/main/java/com/example/planets/PickerText.kt
+++ app/src/main/java/com/example/planets/PickerText.kt
@@ -1,14 +1,23 @@
 package com.example.planets
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType.Companion.Sp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.planets.ui.theme.Orange
+import kotlinx.coroutines.launch
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun PickerText (
     text: String,
@@ -16,18 +25,51 @@ fun PickerText (
 ){
     val isHere = current == text && text.isNotBlank()
 
+    val textSize = remember {
+        Animatable(
+            if (isHere) 28f else 18f
+        )
+    }
+
+    val textSpacing = remember {
+        Animatable(
+            if (isHere) 4f else 2f
+        )
+    }
+    val textVer = remember {
+        Animatable(
+            if (isHere) 32f else 16f
+        )
+    }
+    val textStart = remember {
+        Animatable(
+            if (isHere) 80f else 96f
+        )
+    }
+
+    val scope = rememberCoroutineScope()
+
+    scope.launch {
+        textSpacing.animateTo(targetValue = if (isHere) 4f else 2f, animationSpec = tween(durationMillis = 50))
+    }
+    scope.launch {
+        textSize.animateTo(targetValue = if (isHere) 28f else 18f, animationSpec = tween(durationMillis = 50))
+    }
+    scope.launch {
+        textVer.animateTo(targetValue = if (isHere) 32f else 16f, animationSpec = tween(durationMillis = 50))
+    }
+    scope.launch {
+        textStart.animateTo(targetValue = if (isHere) 80f else 96f, animationSpec = tween(durationMillis = 50))
+    }
+
     Text(
         modifier = Modifier
-            .padding(16.dp)
-            .padding(
-                start = if (isHere) 64.dp else 80.dp,
-                top = if (isHere) 16.dp else 0.dp,
-                bottom = if (isHere) 16.dp else 0.dp
-            ),
+            .padding(vertical = textVer.value.dp)
+            .padding(start = textStart.value.dp),
         text=text,
-        fontSize = if (isHere) 28.sp else 18.sp,
+        fontSize = textSize.value.sp,
         color = Orange,
-        letterSpacing = if (isHere) 4.sp else 2.sp,
+        letterSpacing = textSpacing.value.sp,
         fontWeight = if (isHere) FontWeight.SemiBold else FontWeight.Normal
     )
 }
