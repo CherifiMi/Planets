5ae0e597a7b79eef9fb31ec47ed05ced762f1c1c
CherifiMi <cherifi154949@gmail.com>
2022-07-04 22:24:47 +0100

    the bottom part of the pick screen

---
+++ app/src/main/java/com/example/planets/components/BottomViews.kt
@@ -0,0 +1,30 @@
+package com.example.planets.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.planets.R
+import com.example.planets.ui.theme.Orange
+
+@Composable
+fun ColumnScope.BottomViews() {
+    Row(
+        modifier = Modifier.padding(bottom = 32.dp).weight(1f),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        FloatingActionButton(onClick = { /*TODO*/ }, backgroundColor = Orange) {
+            Icon(
+                painter = painterResource(id = R.drawable.rokit_icon),
+                contentDescription = "")
+        }
+    }
+}
