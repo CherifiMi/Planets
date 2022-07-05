9867c5d456bc1b430a0fca5cdefdb21c856e49d4
CherifiMi <cherifi154949@gmail.com>
2022-07-05 05:54:59 +0100

    put every thing in the PickerTextItem.kt in a box

--- app/src/main/java/com/example/planets/components/PickerTextItem.kt
+++ app/src/main/java/com/example/planets/components/PickerTextItem.kt
@@ -4,13 +4,16 @@ import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType.Companion.Sp
@@ -20,9 +23,32 @@ import com.example.planets.ui.theme.Orange
 import dev.chrisbanes.snapper.SnapperLayoutItemInfo
 import kotlinx.coroutines.launch
 
+@Composable
+fun PickerTextItem(
+    text: String,
+    current: Int?,
+    index: Int
+) {
+    Log.d("HELLO", current.toString())
+    val isHere = (current ?: 0) == index
+
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier = Modifier
+            .fillMaxWidth()
+            .offset(y = (-152).dp)
+            .height(if (isHere) 144.dp else 48.dp)
+            .background(if (isHere) Color.Black else Color.Blue)
+            .border(2.dp, Color.White)
+    ){
+        _PickerTextItem(text = text, current = current, index = index)
+    }
+}
+
+
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
-fun PickerTextItem (
+fun _PickerTextItem (
     text: String,
     current: Int?,
     index: Int
@@ -50,25 +76,24 @@ fun PickerTextItem (
         )
     }
 
+    val animeSpeed = 60
     val scope = rememberCoroutineScope()
 
     scope.launch {
-        textSpacing.animateTo(targetValue = if (isHere) 4f else 2f, animationSpec = tween(durationMillis = 50))
+        textSpacing.animateTo(targetValue = if (isHere) 4f else 2f, animationSpec = tween(durationMillis = animeSpeed))
     }
     scope.launch {
-        textSize.animateTo(targetValue = if (isHere) 28f else 17f, animationSpec = tween(durationMillis = 50))
+        textSize.animateTo(targetValue = if (isHere) 28f else 17f, animationSpec = tween(durationMillis = animeSpeed))
     }
     scope.launch {
-        textVer.animateTo(targetValue = if (isHere) 32f else 16f, animationSpec = tween(durationMillis = 50))
+        textVer.animateTo(targetValue = if (isHere) 32f else 16f, animationSpec = tween(durationMillis = animeSpeed))
     }
     scope.launch {
-        textStart.animateTo(targetValue = if (isHere) 72f else 88f, animationSpec = tween(durationMillis = 50))
+        textStart.animateTo(targetValue = if (isHere) 72f else 88f, animationSpec = tween(durationMillis = animeSpeed))
     }
 
     Text(
-        modifier = Modifier
-            .padding(vertical = textVer.value.dp)
-            .padding(start = textStart.value.dp),
+        modifier = Modifier.padding(start = textStart.value.dp),
         text=text,
         fontSize = textSize.value.sp,
         color = Orange,
